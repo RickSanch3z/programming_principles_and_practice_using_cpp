890371834a9cac91f622efc8dffa7c1cbf9e4dc1
RickSanch3z <codding.test@gmail.com>
2018-08-31 13:23:02 +0000

    Update 8.cpp

--- ch4/exercises/8.cpp
+++ ch4/exercises/8.cpp
@@ -5,10 +5,12 @@ int main()
     int square = 1;
     int c_n_of_grains = 1;
     int t_n_of_grains = 1;
-    int limit_grains = 1;
+    int limit_grains = 0;
     
     cout << "Please enter the number of grains required, at least.\n";
     cin >> limit_grains;
+    if (limit_grains < 1)
+        square = 0;
     
     for (; t_n_of_grains < limit_grains; ++square) {
         c_n_of_grains *= 2;
