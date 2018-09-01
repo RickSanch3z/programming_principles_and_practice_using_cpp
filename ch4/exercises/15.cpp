197ea139cc33c63ffca59771d86e586ddfb76b31
RickSanch3z <codding.test@gmail.com>
2018-09-01 11:57:07 +0000

    Update 15.cpp

--- ch4/exercises/15.cpp
+++ ch4/exercises/15.cpp
@@ -12,26 +12,30 @@ vector<int> primes;
 
 int main()
 {
-    int n = 0;  // user input
-    int n_var = 0;  // actual primes number
-    
+    int n = 0; // user input
+    int n_var = 0; // actual primes number
+
     cout << "Please enter a number of first primes to print: ";
     cin >> n; // ask user to enter a maximum limit to test through
-    primes.push_back(2);    // the smallest prime number is 2
+    primes.push_back(2); // the smallest prime number is 2
     n_var++;
-    
-    for (int i = 3 ; n_var < n; ++n_var) {    // 0 and 1 is not primes, and 2 is already added
-        for ( ; !is_prime(i); ++i); // skip all non-primes
-        
+
+    for (int i = 3; n_var < n; ++n_var) { // 0 and 1 is not primes, and 2 is already added
+        for (; !is_prime(i); ++i); // skip all non-primes
+
         primes.push_back(i++);
     }
-    
+
     // print primes from 1 to max limit included
-    cout << n << " first primes are:\n";
-    for (int i = 0; i < primes.size(); ++i) {
-        cout << primes[i] << " ";
+    if (n == 0)
+        cout << "No primes to print.\n";
+    else {
+        cout << n << " first primes are:\n";
+        for (int i = 0; i < primes.size(); ++i) {
+            cout << primes[i] << " ";
+        }
+        cout << '\n';
     }
-    cout << '\n';
 
     return 0;
 }
